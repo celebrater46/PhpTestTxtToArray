59f14b132e654aeadd6d8dd985dd6b0e55f1e4e1
celebrater <tkgm45nbchk@gmail.com>
2022-01-18 13:28:04 +0900

    test succeeded

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
 // Test TXT to Array
+$file_name = "txt/test.txt";
+$ret_array = file( $file_name );
+$count = count($ret_array);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="Author" content="Enin Fujimi">
+    <title>Test TXT To Array</title>
+</head>
+<body>
+<h1>
+    <a href="/">
+        Test TXT To Array
+    </a>
+</h1>
+    <?php for ($i = 0; $i < $count; $i++) : ?>
+        <?php echo $i.": ".$ret_array[$i] . "<br>"; ?>
+    <?php endfor; ?>
+
+</body>
+</html>
